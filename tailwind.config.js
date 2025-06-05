2e50f965d3829de9e516278ffd25c29926cb4327
Andi Samsualam <andi.samsualam95@gmail.com>
2025-06-05 17:47:45 +0700

    responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './index.html',
-    './src/**/*.{js,jsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#33CCCC',
+        'primary-dark': '#2bb8b8',
+        'text-primary': '#000000',
+        'text-secondary': '#586068',
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'roboto': ['Roboto', 'sans-serif'],
+      },
+      container: {
+        center: true,
+        padding: '1rem',
+      },
+    },
   },
   plugins: [],
 }
